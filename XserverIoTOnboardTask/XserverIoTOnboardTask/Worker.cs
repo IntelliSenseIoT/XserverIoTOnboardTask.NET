e44deaea3313a35bd62c80b4fb8b427763615557
IntelliSense <intellisenseltd@outlook.com>
2023-05-08 11:02:41 +0200

    First version

--- XserverIoTOnboardTask/XserverIoTOnboardTask/Worker.cs
+++ XserverIoTOnboardTask/XserverIoTOnboardTask/Worker.cs
@@ -1,9 +1,24 @@
+using IO.NET.SimpleHttpServer;
 using Microsoft.Extensions.Hosting.Systemd;
+using Service.Common.NET;
+using XserverIoTCommon;
 
 namespace XserverIoTOnboardTask
 {
     public class Worker : BackgroundService
     {
+        #region XServerIoTOnboardTask service settings
+        //Service display name
+        private const string ServiceDisplayName = "Xserver.OnboardTask";
+        //Task Handler Period (ms)
+        private const int TaskHandlerPeriod = 1000;
+        #endregion
+
+        #region Helpers
+        HttpRestServerService RestServer = new HttpRestServerService();
+        Application serviceinfo = new Application();
+        #endregion
+
         #region Service Logger
         private readonly ILogger<Worker> _logger;
 
@@ -13,9 +28,13 @@ namespace XserverIoTOnboardTask
         }
         #endregion
 
-
         public override Task StartAsync(CancellationToken cancellationToken)
         {
+            var StartupTimeUTC = DateTime.UtcNow;
+            _logger.LogInformation("Start "+ ServiceDisplayName + " service - " + StartupTimeUTC);
+            var appinfo = serviceinfo.GetInfo();
+            _logger.LogInformation("Version: " + appinfo.ApplicationVersion);
+
             //Start initializing
             Initialize();
 
@@ -23,20 +42,88 @@ namespace XserverIoTOnboardTask
         }
 
         /// <summary>
-        /// Start Core Service initialize
+        /// Start OnboardTask Service initialize
         /// </summary>
         private async void Initialize()
         {
+            await EventLogging.AddLogMessage(MessageType.Info, this.GetType().Name + " - " + ServiceDisplayName + " - " + "Start initializing...");
 
+            try
+            {
+                //Initialize Http REST server
+                await RestServer.HttpRESTServerStart();
+                RestServer.ClientEvent += HttpRestServer_ClientRequestEvent;
 
+                //Todo: Write your initial code here
+                //_logger.LogInformation("Debug message");
+            }
+            catch (Exception ex)
+            {
+                await EventLogging.AddLogMessage(MessageType.ExceptionError, this.GetType().Name + " - " + ServiceDisplayName + " - " + "Initialization exception error! Error: " + ex.Message);
+            }
+
+            await EventLogging.AddLogMessage(MessageType.Info, this.GetType().Name + " - " + ServiceDisplayName + " - " + "Finished initialization.");
         }
 
+        /// <summary>
+        /// Periodic Task
+        /// </summary>
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                try
+                {
+                    //Todo: Type your task code here
+                    //_logger.LogInformation("Debug, Task running at: {time}", DateTimeOffset.Now);
+                }
+                catch (Exception ex)
+                {
+                    await EventLogging.AddLogMessage(MessageType.ExceptionError, this.GetType().Name + " - " + ServiceDisplayName + " - " + "OnboardTask exception error! Error: " + ex.Message);
+                }
+
+                await Task.Delay(TaskHandlerPeriod, stoppingToken);
+            }
+        }
+
+        private async void HttpRestServer_ClientRequestEvent(object sender, HttpRestServerService.ClientRequestEventArgs e)
+        {
+            IO.NET.SimpleHttpServer.Result res = new IO.NET.SimpleHttpServer.Result();
+
+            try
+            {
+                if (e.RequestMethod == RequestMethodType.GET)
+                {
+                    //Todo: Type your code here
+                    //_logger.LogInformation("Debug message");
+                    // Example:
+                    //if (e.uriString.ToLower() == "/onboardtask/examplegeturi")
+                    //{
+                    //    string content = JsonConvert.SerializeObject(YourObject);
+                    //    res = await RestServer.ServerResponse(HTTPStatusCodes.OK, e.OStream, content);
+                    //}
+                }
+                else if (e.RequestMethod == RequestMethodType.POST)
+                {
+                    //Todo: Type your code here
+                    //_logger.LogInformation("Debug message");
+                    // Example:
+                    //if (e.uriString.ToLower() == "/onboardtask/exampleposturi")
+                    //{  
+                    //    YourObject MyObj = JsonConvert.DeserializeObject<YourObject>(e.HttpContent);
+                    //    ....
+                    //    string content = JsonConvert.SerializeObject(answer);
+                    //    res = await RestServer.ServerResponse(HTTPStatusCodes.OK, e.OStream, content);
+                    //}
+                }
+                else
+                {
+                    res = await RestServer.ServerResponse(HTTPStatusCodes.Not_Found, e.OStream, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                await EventLogging.AddLogMessage(MessageType.ExceptionError, this.GetType().Name + " - " + ServiceDisplayName + " - " + "Http REST server exception error! Error: " + ex.Message);
             }
         }
     }
